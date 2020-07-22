b0a0e502e38fa0126472b84ccda21fe005740166
dodget <tylor_dodge@harvard.edu>
2020-07-22 12:23:13 -0400

    Fix bug in data view and add some tests

--- frontend/src/components/utils.spec.js
+++ frontend/src/components/utils.spec.js
@@ -6,11 +6,13 @@ const heatMapData = pivotHeatMapData(testPayload);
 
 describe('utility functions', () => {
   it('returns true when criterion can be flattened', () => {
+    // in this set of datapoints, all rating descriptions are the same
     const rubric = heatMapData[0].dataPoints;
     expect(flatData(rubric)).toEqual(true);
   });
 
   it('returns false when criterion cannot be flattened', () => {
+    // in this set of datapoints, all rating descriptions are NOT the same
     const rubric = heatMapData[1].dataPoints;
     expect(flatData(rubric)).toEqual(false);
   });
